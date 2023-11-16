1abc7bbe3a21b6f22539af169b16f74c0151de4f
TaylorC19 <96517792+TaylorC19@users.noreply.github.com>
2023-11-16 14:13:21 +0900

    feat: added contributions to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import Footer from "./components/footer";
 import GamerTalk from "./components/Gamertalk";
 import RecipeNutriShare from "./components/RecipeNutriShare";
+import { poppins } from "./layout";
 
 export default function Home() {
   const [lang, setLang] = useState<string>("English");
@@ -43,12 +44,42 @@ export default function Home() {
         </a>
       </div>
 
+      <h2 className="text-4xl pt-10 flex flex-col items-center justify-center underline mb-5">
+        {lang === "English" ? "Projects" : "プロジェクト"}
+      </h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-5 text-4xl">
         <RecipeNutriShare lang={lang} />
 
         <GamerTalk lang={lang} />
       </div>
 
+      <div>
+        <h2 className="text-4xl pt-10 flex flex-col items-center justify-center underline">
+          {lang === "English" ? "Contributions" : "フリーランスの仕事"}
+        </h2>
+
+        <div className="my-5 flex flex-col items-center justify-center rounded-xl">
+          <Image
+            src="/images/mamoru.png"
+            width={70}
+            height={70}
+            alt="Mamoru Icon"
+          />
+          <p className={poppins.className}>mamoru</p>
+          <a
+            className="text-blue-500 underline"
+            href="http://app.mamoru.earth"
+            target="_blank"
+          >
+            http://app.mamoru.earth
+          </a>
+          <p className="p-10">
+            {lang === "English"
+              ? "mamoru is a sustainable living app that helps you discover and save on sustainable products, shops, cafes, restaurants, and more."
+              : "mamoru（まもる）は、サステナブルな商品、ショップ、カフェ、レストランなどを発見し、お得に利用することができる サステナブルライフアプリです。"}
+          </p>
+        </div>
+      </div>
       <Footer
         text={
           lang === "English"
